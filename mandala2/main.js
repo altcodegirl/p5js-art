ac0f8d54a7f6c15df7d6ff97ad09c61aad4232f4
altcodegirl <emma.k.donnelly@gmail.com>
2017-12-07 16:23:34 +0000

    Update main.js

--- mandala2/main.js
+++ mandala2/main.js
@@ -1,7 +1,10 @@
 var r;
+var backgroundColor;
+var isOverRectangle;
 
- function setup() {
+function setup() {
   createCanvas(windowWidth, windowHeight);
+  backgroundColor = color(255);
   fill(0);
   stroke(0);
   r = color (0);
@@ -9,11 +12,45 @@ var r;
 
 function draw()
 {
+  background(backgroundColor);
+	// display instructions for top bar
 	noStroke();
 	fill(200);
 	rect(0, 0, width, 25);
 	fill(0);
-	text("press r for red", 10, 15);
+	text("draw your mandala", 10, 15);
+
+  // check if mouse is inside the rectangle
+  // mouseX >= x && mouseX <= x+width && mouseY >= y && mouseY <= y+height
+  if (mouseX >= 150 && mouseX <= 150+100 && mouseY >= 150 && mouseY <= 150+100)
+  {
+    isOverRectangle = true;
+  } else {
+    isOverRectangle = false;
+  }
+
+  // draw a rectangle
+  rectMode(CORNER);
+  stroke(0);
+  strokeWeight(5);
+  if(isOverRectangle == true)
+  {
+    fill(100);
+    cursor(HAND);
+  } else {
+	fill(200);
+	cursor(ARROW);
+  }
+  rect(150, 150, 100, 100);
+
+}
+
+function mousePressed()
+{
+  if(isOverRectangle == true)
+  {
+    backgroundColor = color(random(255));
+  }
 }
 
 function mouseDragged()
